b92a3fd06bee44122ce12d5f670812a3143a8ae1
keane3192 <55885290+keane3192@users.noreply.github.com>
2021-05-30 13:41:35 +0300

    feat: added solution

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,22 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var x = sadArray.size-2
+        var sadArray2 = sadArray
+        var i = 1
+        while (i <= x){
+            if (sadArray2[i] > (sadArray2[i-1] + sadArray2[i+1])){
+                var sadArray3 = sadArray2.filterIndexed { index, _ ->
+                    index != i
+                }
+                sadArray2 = sadArray3.toIntArray()
+                x--
+                if(i != 1) {i--}
+            } else{
+                i++
+            }
+        }
+        return sadArray2
     }
 }
+
