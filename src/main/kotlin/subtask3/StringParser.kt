f74758c18ce69730fb2b0b8045b6f908cb784077
keane3192 <55885290+keane3192@users.noreply.github.com>
2021-05-30 13:43:51 +0300

    feat: added solution

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,48 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var arr2 : MutableList<String> = mutableListOf<String>()
+        var charr = "<[(".toCharArray()
+        var x = 0
+        while(inputString.indexOfAny(charr, x) != -1){
+            var indxchar = inputString.indexOfAny(charr, x)
+            x = indxchar + 1
+            var check = -1
+            if(inputString[indxchar].toString().equals("<",true)){
+                check = checkIn("<",">",inputString,x)
+                if (check != -1){
+                    arr2.add(inputString.substring(x, check))
+                }
+
+            }
+            else if(inputString[indxchar].toString().equals("[",true)){
+                check = checkIn("[","]",inputString,x)
+                if (check != -1){
+                    arr2.add(inputString.substring(x, check))
+                }
+            }
+            else if(inputString[indxchar].toString().equals("(",true)){
+                check = checkIn("(",")",inputString,x)
+                if (check != -1){
+                    arr2.add(inputString.substring(x, check))
+                }
+            }
+        }
+        return arr2.toTypedArray()
+    }
+
+    fun checkIn(open: String, close: String, inputString: String, x:Int):Int{
+
+        var close1 = inputString.indexOf(close, x)
+        var inopen1 = inputString.indexOf(open, x)
+
+        while(inopen1 != -1 && inopen1 < close1){
+            close1 = inputString.indexOf(close, close1+1)
+            inopen1 = inputString.indexOf(open, inopen1+1)
+        }
+        return close1
+
+
+
     }
 }
